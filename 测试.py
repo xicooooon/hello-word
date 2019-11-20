0c964dca40eba5329b9faed43fa0f5db2b6502cb
xicooooon <mxcchero@163.com>
2019-11-20 22:57:59 +0800

    First Commit

---
+++ 测试.py
@@ -0,0 +1,17 @@
+import graphviz
+import sklearn.datasets as datasets
+import pandas as pd
+iris=datasets.load_iris()
+df=pd.DataFrame(iris.data, columns=iris.feature_names)
+y=iris.target
+from sklearn.tree import DecisionTreeClassifier
+dtree=DecisionTreeClassifier()
+dtree.fit(df, y)
+from sklearn.externals.six import StringIO
+from IPython.display import Image
+from sklearn.tree import export_graphviz
+import pydotplus
+dot_data=StringIO()
+export_graphviz(dtree,out_file=dot_data,filled=True, rounded=True, special_characters=True)
+graph=pydotplus.graph_from_dot_data(dot_data.getvalue())
+Image(graph.create_png())
